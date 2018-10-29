9e7a8e58450f39c0e444058dbd4b00dfcc0fd470
dendi <dendi@networkws.com>
2018-10-29 11:20:23 +0800

    change deployment/setting.py file

--- deloyment/settings.py
+++ deloyment/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'deloyment.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
+# sqlite config
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -86,6 +86,7 @@ DATABASES = {
     }
 }
 
+# Mysql config
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
