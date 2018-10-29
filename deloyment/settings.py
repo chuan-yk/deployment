24c08c0b5185b5ca16c827381040d93cc41c14e0
dendi <dendi@networkws.com>
2018-10-29 11:32:08 +0800

    use mysql config again

--- deloyment/settings.py
+++ deloyment/settings.py
@@ -79,24 +79,24 @@ WSGI_APPLICATION = 'deloyment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 # sqlite config
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# Mysql config
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'django',
-#         'USER': 'django',
-#         'PASSWORD': 'django',
-#         'HOST': '10.46.5.246',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+# Mysql config
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '10.46.5.246',
+        'PORT': '3306',
+    }
+}
 
 
 
