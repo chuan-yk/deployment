eb00a04f3cf4b7a63d245f20b4fd3940c1162eb7
dendi <dendi@networkws.com>
2018-10-29 11:40:07 +0800

    change setting

--- deloyment/settings.py
+++ deloyment/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'deloyment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 # sqlite config, dendi local env dev
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Mysql config
 DATABASES = {
