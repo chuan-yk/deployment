0a0aa8f8340819b77fe0768c521a28c182abfd4d
dendi <dendi@networkws.com>
2018-11-13 15:58:25 +0800

    待发布文件详情页面

--- frontitems/urls.py
+++ frontitems/urls.py
@@ -5,10 +5,11 @@ from . import views
 app_name = 'frontitems'
 urlpatterns = [
 
-    url(r'^upload/$', views.upload, name='upload'),
-    url(r'^list/$', views.list, name='list_all'),
-    url(r'^list/(?P<pk>[0-9]+)/detail/$', views.list_detail, name='list_pub'),
-    #url(r'^pub/(?P<pk>[0-9]+)/do$', views.pub, name='do_pub'),
-    #url(r'^pub/(?P<pk>[0-9]+)/result$', views.pubresult, name='pub_result'),
-
+    url(r'^upload/$', views.upload, name='upload'),                                 # 原一键发布入口，弃用
+    url(r'^list/$', views.list, name='list_all'),                                   # 列表详情
+    url(r'^list/(?P<pk>[0-9]+)/detail/$', views.list_detail, name='file_detail'),   # 文件详情
+    url(r'^pub/(?P<pk>[0-9]+)/do$', views.pub, name='do_pub'),                      # 执行发布任务
+    url(r'^pub/(?P<pk>[0-9]+)/detail$', views.pubresult, name='pub_detail'),        # 发布状态查看，发布进度和过程
+    url(r'^pub/(?P<pk>[0-9]+)/result$', views.pubresult, name='pub_result'),        # 发布结果， 更新文件详情
+    url(r'^pub/(?P<pk>[0-9]+)/return$', views.pubresult, name='pub_return'),        # 回滚接口
 ]
--- frontitems/views.py
+++ frontitems/views.py
@@ -1,38 +1,56 @@
 import os
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-#from django.http import HttpResponse, HttpResponseRedirect
+# from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 import threading
 from datetime import datetime, date, timedelta
 from .models import ProjectInfo, RecordOfStatic
 from django.core.exceptions import ObjectDoesNotExist
-
+from django.http import HttpResponse
 from fileupload.models import Fileupload
 from .remotepubstatic import RemoteReplaceWorker
 
+
 @login_required
 def list(request):
-    #static_uploadfile_list = Fileupload.objects.filter(pk=1)
+    """静态文件全部内容列表
+       Fileupload: 0 为静态文件
+    """
     static_uploadfile_list = Fileupload.objects.filter(type=0)
     return render(request, 'frontitems/list.html', {'static_uploadfile_list': static_uploadfile_list})
 
 
 @login_required
 def list_detail(request, pk):
-    pub_detail = RecordOfStatic.objects.get(pk=pk)
-    return render(request, 'frontitems/detail.html', {'pub_detail':pub_detail})
+    """指定pk 文件详情, Get参数filemd5 为1: 显示更新文件MD5 信息，"""
+    if request.GET.get('filemd5') == '1':
+        show = True
+        MD5 = {'f1': '1111111', 'f2': '22222222', 'f3': '3333333333'}
+    else:
+        show = False
+        MD5 = {'None': None}
+
+    uploadfile_detail = Fileupload.objects.get(pk=pk)
+    # pub_lock   = ''  # function return
+    pub_lock = {'lock': False , 'pubtask': 'mc_sobet_jar_20181108_0001'}
+    filemd5 = {'show': show, 'MD5': MD5}
+    context = {'uploadfile_detail': uploadfile_detail, 'pub_lock': pub_lock, 'filemd5': filemd5}
+    print(context['filemd5'])
+    return render(request, 'frontitems/file_detail.html', context)
 
 
 @login_required
 def pub(request, pk):
-    pass
+    print(pk)
+    return HttpResponse('it is ok , test')
 
 
 @login_required
 def pubresult(request, pk):
     pass
+
 
 @login_required
 def run_tasker(task_projectinfo, inputfiledir):
@@ -46,11 +64,12 @@ def run_tasker(task_projectinfo, inputfiledir):
     time.sleep(10)
     # tasker.ignore_newfile_run()
 
+
 @login_required
-def upload(request):
+def upload(request):  # 原一键发布入口，弃用
     project_cn_set = set()
     platformlist = {}
-    items=[]
+    items = []
     for project_cn in ProjectInfo.objects.values_list('project_cn'):
         project_cn_set.add(project_cn[0])
     for pjt_cn in project_cn_set:
@@ -62,7 +81,7 @@ def upload(request):
     items.sort()
     # platformlist = {'摩臣':"mc", '摩臣2':"mc2", '摩登':"md"}
     # items = ['sobet', 'lottery', 'lottery_m', 'sport']
-    context = { 'platformlist': platformlist,  'items': items, }
+    context = {'platformlist': platformlist, 'items': items, }
 
     if request.method == "POST":  # 请求方法为POST时，进行处理
         try:
@@ -73,7 +92,7 @@ def upload(request):
             task_projectinfo = ProjectInfo.objects.get(project=pst_platform, items=pst_item)
             if task_projectinfo.package_name != pst_filename:
                 raise FileNotFoundError('Upload file = {}  does not match {}'.format(pst_filename,
-                                                                                    task_projectinfo.package_name))
+                                                                                     task_projectinfo.package_name))
             inputfiledir = os.path.join(task_projectinfo.file_save_base_dir,
                                         datetime.now().strftime('%Y%m%d_%H%M%S'))  # linux run
             # inputfiledir = 'D:\\static' # window debug
@@ -86,9 +105,9 @@ def upload(request):
             task = threading.Thread(target=run_tasker, args=(task_projectinfo, inputfiledir))
             task.start()
             messages.success(request, '发布成功！', 'alert-success')
-            #if tasker.coversuccess:
-            #print('===-======debug  2')
-            #coversuccess = True
+            # if tasker.coversuccess:
+            # print('===-======debug  2')
+            # coversuccess = True
             # if coversuccess:    #tasker.coversuccess:
             #     messages.success(request, '发布成功！', 'alert-success')
             #     print('===-======debug  3')
@@ -111,7 +130,7 @@ def upload(request):
             #     messages.warning(request, "默认新增文件不跟新，请联系运维或项目主管审核后更新", 'alert-danger' )
             #     context['newfiles'] = ['static/new/1.txt', '/static/new/mew.image.png'] #'tasker.newfilelist  '
             #     context['newdirs'] = ['static/newimage', 'static/download/']#'tasker.newdirlist'
-        except AttributeError as e:   #myFile.name
+        except AttributeError as e:  # myFile.name
             messages.error(request, '请选择更新文件', 'alert-danger')
         except FileNotFoundError as e:
             messages.error(request, '上传文件不正确\n{}'.format(str(e)), 'alert-danger')
@@ -120,17 +139,16 @@ def upload(request):
             print(e)
         except Exception as e:
             print(e)
-            messages.error(request, '未知错误： '+str(e), 'alert-danger')
+            messages.error(request, '未知错误： ' + str(e), 'alert-danger')
         finally:
             return render(request, 'frontitems/upload.html', context)
-            #return redirect(reverse(upload))
+            # return redirect(reverse(upload))
             pass
 
     elif request.method == 'GET':
         # if request.user.is_authenticated():
         #     #print(request.user.username, '====')
         #     pass
-        #return HttpResponse(template.render(context, request))
+        # return HttpResponse(template.render(context, request))
         context = {'platformlist': platformlist, 'items': items, }
-        return render(request,'frontitems/upload.html',context)
-
+        return render(request, 'frontitems/upload.html', context)
