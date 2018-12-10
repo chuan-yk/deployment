83267223847f83d49af323f2ddc567c1dac6a0db
shine <shine@networws.com>
2018-12-10 15:25:15 +0800

    小更新

--- autojar/urls.py
+++ autojar/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 
     url(r'delfile/$', views.delfile, name='del_file'),
     url(r'^deploy_list/$',views.deploy_list.as_view(), name='deploy_list'),
+    url(r'^history_list/$',views.history_list.as_view(), name='history_list'),
     url(r'RunEnter/$', views.RunEnter, name='RunEnter'),
 
 ]
